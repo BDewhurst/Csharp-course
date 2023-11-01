f30b63a6cbfa625034bbe6a824af8234901f07e9
Brad <bradleydewhurst123@outlook.com>
2023-11-01 16:05:44 +0000

    conversion

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using Microsoft.VisualBasic;
 
 public class Lecture {
     int age = 15;
-
+    string myname = "brad";
+    double myDouble = 13.37;
+    int myInt;
     public void DisplayAge() {
         Console.WriteLine("Hello, Dennis!");
         Console.WriteLine("Age: " + age);
+        Console.WriteLine("My name is " + myname);
+        Console.WriteLine(myname.ToUpper());
+        myInt = (int)myDouble;
+        Console.WriteLine(myInt);
+
     }
 
     public static void Main(string[] args) {
