14d24d66a824f6bceecb8e0e569502dec4d5537f
Brad <bradleydewhurst123@outlook.com>
2023-11-01 10:34:28 +0000

    display age

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Dennis!");
+using System;
+
+public class Lecture {
+    int age = 15;
+
+    public void DisplayAge() {
+        Console.WriteLine("Hello, Dennis!");
+        Console.WriteLine("Age: " + age);
+    }
+
+    public static void Main(string[] args) {
+        Lecture lecture = new Lecture();
+        lecture.DisplayAge();
+    }
+}
