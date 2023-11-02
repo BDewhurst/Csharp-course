ee7ba05fa2f0871497345c2ee9332aab18477367
Brad <bradleydewhurst123@outlook.com>
2023-11-02 09:26:50 +0000

    Assignment 1

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ public class Lecture {
     int age = 15;
     string myname = "brad";
     double myDouble = 13.37;
+    float myFloat = 13.23f;
+    
     int myInt;
     public void DisplayAge() {
         Console.WriteLine("Hello, Dennis!");
@@ -17,8 +19,20 @@ public class Lecture {
 
     }
 
-    public static void Main(string[] args) {
-        Lecture lecture = new Lecture();
-        lecture.DisplayAge();
-    }
+   // public static void Main(string[] args) {
+    //    Lecture lecture = new Lecture();
+      //  lecture.DisplayAge();
+   // }
+}
+
+public class Assignment1 {
+       public static void Main(string[] args)
+    {   
+       string stringForFloat = "0.85"; // datatype should be float
+       string stringForInt = "12345"; // datatype should be int
+       float floatValue = float.Parse(stringForFloat);
+       int intValue = int.Parse(stringForInt);
+        Console.WriteLine(intValue);
+        Console.WriteLine(floatValue);
+    }  
 }
