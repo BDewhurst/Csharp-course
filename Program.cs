87d7a41f1b5c845d8e89f8b1d4bbd5c107567e4f
Brad <bradleydewhurst123@outlook.com>
2023-11-09 15:40:54 +0000

    arrays

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ using Microsoft.VisualBasic;
 
 internal class Program
 {
+
+  static int[,] matrix = {
+    {1,2,3}, 
+    {4,5,6}, 
+    {7,8,9}
+    };
  static void Main(string[] args) {
+  foreach (int item in matrix) {
+    Console.WriteLine(item);
+  }
+
+  for (int i = 0; i < matrix.GetLength(0); i++) {
+    for (int j = 0; j < matrix.GetLength(1); j++) {
+      Console.WriteLine(matrix[i, j]);
+    }
+  }
+
   Car myCar = new Car();
   Car audi = new Car("audi", 350);
   Car bmw = new Car("bmw");
